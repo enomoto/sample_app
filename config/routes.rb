08f1b94b357844a31c84d43589a5e58ca0f1423e
enomoto <knt527@gmail.com>
2014-06-18 00:25:27 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 SampleApp::Application.routes.draw do
-#  get "users/new"
-#  get "static_pages/home"
-#  get "static_pages/help"
-#  get "static_pages/about"
-#  get "static_pages/contact"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions,   only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/signin', to: 'sessions#new', via: 'get'
